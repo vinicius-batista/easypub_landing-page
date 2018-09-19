f48a10811e2feeaa9793d474e88e749ef0b5447c
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-09-19 08:14:11 -0300

    main: update secundary text

--- components/main/MainContent.vue
+++ components/main/MainContent.vue
@@ -8,7 +8,7 @@
       <h3 class="white--text font-weight-bold display-2 mb-3">
         Tenha um atendimento rápido e livre de erros com cardápios mais completos e intuitivos!
       </h3>
-      <h6 class="white--text font-weight-regular title" style="line-height: 1.3 !important;">
+      <h6 class="grey--text text--lighten-2 font-weight-regular title" style="line-height: 1.3 !important;">
         Todas as informações do bar no seu celular com EasyPub. Evite erros causados por garçons e pela falta de informações em cardápios convencionais.
       </h6>
       <SubscribeBtn class="mt-5"/>
