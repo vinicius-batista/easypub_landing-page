059c9e33839a66a748e48f591f75a93433d44b1b
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-09-19 19:15:59 -0300

    main: update about photo

--- components/main/About.vue
+++ components/main/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div id="about"></div>
+  <v-layout
+    id="about"
+    justify-center
+    align-center
+    wrap
+    class="my-5"
+  >
+    <v-flex xs12 md4>
+      <v-img :src="require('~/assets/images/about-min.png')"></v-img>
+    </v-flex>
+    <v-flex xs10 md4>
+      <h2 class="primary--text display-1 font-weight-regular font-italic">
+         Você é seu próprio garçom!
+      </h2>
+      <h4 class="blue-grey--text text--darken-1 title mt-5">
+        Veja todos os detalhes dos pratos oferecidos pela casa e escolha o preferido quando quiser.
+      </h4>
+      <br>
+      <h4 class="blue-grey--text text--darken-1 title mb-5">
+        Nunca mais passe por situações desagradáveis como espera para ser atendido e eventuais erros. Confira o valor de sua comanda em tempo real.
+      </h4>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
