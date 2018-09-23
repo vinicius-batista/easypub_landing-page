f6be4a8ac22ed705b0581bdaaaf6e17641dc7a8c
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-09-23 09:21:04 -0300

    meta: add meta tags for social media sharing

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,41 @@
+const title = 'EasyPub - Soluções digitais para bares'
+const description = 'EasyPub é uma plataforma criada para agilizar o atendimento em bares com cardápios, comandas e pagamentos digitais.'
+const url = 'https://easypub.com.br/'
 
 module.exports = {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'EasyPub - Soluções digitais para bares',
+    title,
     meta: [
       { charset: 'utf-8' },
       { name: 'lang', content: 'pt_BR' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'EasyPub é uma plataforma criada para agilizar o atendimento em bares com cardápios, comandas e pagamentos digitais.' }
+      { hid: 'description', name: 'description', content: description },
+      { property: 'og:title', content: title },
+      { property: 'og:site_name', content: 'EasyPub' },
+      // The list of types is available here: http://ogp.me/#types
+      { property: 'og:type', content: 'website' },
+      // Should the the same as your canonical link, see below.
+      { property: 'og:url', content: url },
+      // { property: 'og:image', content: 'https://easypub.com.br/my-special-image.jpg' },
+      // Often the same as your meta description, but not always.
+      { property: 'og:description', content: description },
+
+      // Twitter card
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: url },
+      { name: 'twitter:title', content: title },
+      { name: 'twitter:description', content: description },
+      // Your twitter handle, if you have one.
+      // { name: 'twitter:creator', content: '@alligatorio' },
+      // { name: 'twitter:image:src', content: 'https://www.my-site.com/my-special-image.jpg' },
+
+      // Google / Schema.org markup:
+      { itemprop: 'name', content: title },
+      { itemprop: 'description', content: description }
+      // { itemprop: 'image', content: 'https://www.my-site.com/my-special-image.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
